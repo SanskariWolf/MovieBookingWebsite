aed6ef19478b7e026db122a59905fe3627ddcd1f
SanskariWolf <tanushpurwar@gmail.com>
2024-08-17 03:55:34 +0530

    Header Section Design Completion Done

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageBannerComponent } from './home-page-banner/home-page-banner.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageBannerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/home-page-banner/home-page-banner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page-banner',
+  templateUrl: './home-page-banner.component.html',
+  styleUrl: './home-page-banner.component.css'
+})
+export class HomePageBannerComponent {
+
+}
